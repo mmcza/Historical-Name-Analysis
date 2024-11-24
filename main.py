f8d7e05643eb3ecd90a668850e7ae4f05f21fdde
mmcza <marcin4c.mc@gmail.com>
2024-11-24 17:25:40 +0100

    Finished all tasks (but no answers to plots)

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ import numpy as np
 import glob
 import matplotlib.pyplot as plt
 import os
+import sqlite3
 
+# Task 4
+def calculate_frequency(df):
+    df['total_births_by_sex'] = df.groupby(['year', 'sex'])['count'].transform('sum')
+    df['frequency_male'] = np.where(df['sex'] == 'M', df['count'] / df['total_births_by_sex'], 0)
+    df['frequency_female'] = np.where(df['sex'] == 'F', df['count'] / df['total_births_by_sex'], 0)
+    return df
+
+# Task 6
 def calculate_top_n_names(df, n):
-    # Task 6
+
     number_of_years = df['year'].nunique()
     names_df = df.groupby(['name', 'sex'])[['frequency_male', 'frequency_female']].sum() / number_of_years
 
@@ -16,27 +25,53 @@ def calculate_top_n_names(df, n):
     top_female_names = names_df.nlargest(n, 'frequency_female')
     top_names = pd.concat([top_male_names, top_female_names])
 
-    # Task 8
+    return top_names
+
+# Task 8
+def calculate_name_diversity(df, top_names, n, country):
     df = df.set_index(['name', 'sex'])
     df['in_top'] = df.index.isin(top_names.index)
     df.reset_index(inplace=True)
-    return top_names, df
 
-def calculate_name_gender_ratio(df, start_year, end_year):
+    diversity_pt = df.pivot_table(index=['year', 'sex'], columns='in_top', values='count', aggfunc='sum', fill_value=0)
+    diversity_pt['top_percentage'] = diversity_pt[True] / (diversity_pt[True] + diversity_pt[False])
+
+    reshaped = diversity_pt['top_percentage'].unstack(level='sex')
+    reshaped['difference'] = abs(reshaped['M'] - reshaped['F'])
+
+    max_diff_year = reshaped['difference'].idxmax()
+    # max_diff_value = reshaped.loc[max_diff_year, 'difference']
+
+    plt.figure(figsize=(10, 4))
+    plt.plot(reshaped.index, reshaped['M'], 'b', label='Male names')
+    plt.plot(reshaped.index, reshaped['F'], 'r', label='Female names')
+    plt.axvline(max_diff_year, color='g', label='Year with the greatest difference in diversity')
+    plt.title(f'Percentage of names in the top{n} ranking in {country}')
+    plt.xlabel('Year')
+    plt.ylabel('Percentage')
+    plt.legend()
+    return df, reshaped
+
+# Task 10
+def calculate_name_gender_ratio(df, start_year, end_year, only_top):
     # Drop rows where year is not in the specified range
     filtered_df = df[df['year'].between(start_year, end_year)]
 
     grouped_birth_name_df = filtered_df.groupby(['name', 'sex'])[['count', 'in_top']].sum().fillna(0)
     grouped_birth_name_df = grouped_birth_name_df.unstack(level='sex')
-    grouped_birth_name_df = grouped_birth_name_df[grouped_birth_name_df[('in_top', 'F')] + grouped_birth_name_df[('in_top', 'M')] > 0]
+    if only_top:
+        grouped_birth_name_df = grouped_birth_name_df[grouped_birth_name_df[('in_top', 'F')] + grouped_birth_name_df[('in_top', 'M')] > 0]
     grouped_birth_name_df['p_m'] = grouped_birth_name_df[('count', 'M')] / (grouped_birth_name_df[('count', 'M')] + grouped_birth_name_df[('count', 'F')])
     grouped_birth_name_df['p_f'] = 1 - grouped_birth_name_df['p_m']
-    grouped_birth_name_df = grouped_birth_name_df.drop(columns=[('count', 'F'), ('count', 'M'), ('in_top', 'F'), ('in_top', 'M')])
-    grouped_birth_name_df.to_csv('names_ratio_old.csv')
+    if only_top:
+        grouped_birth_name_df = grouped_birth_name_df.drop(columns=[('count', 'F'), ('count', 'M'), ('in_top', 'F'), ('in_top', 'M')])
 
     return grouped_birth_name_df
 
 def main():
+
+    test_time = pd.Timestamp.now()
+
     # 1. Load the data from all files to a single pandas DataFrame
 
     usa_files = glob.glob(os.path.join('data', 'names', 'yob*.txt'))
@@ -69,9 +104,7 @@ def main():
     # 4. Create new columns ("frequency_male" and "frequency female"), and determine the popularity of each name in each
     #    year by dividing number of times the name was given by the total number of births for that gender in that year
 
-    usa_df['total_births_by_sex'] = usa_df.groupby(['year', 'sex'])['count'].transform('sum')
-    usa_df['frequency_male'] = np.where(usa_df['sex'] == 'M', usa_df['count'] / usa_df['total_births_by_sex'], 0)
-    usa_df['frequency_female'] = np.where(usa_df['sex'] == 'F', usa_df['count'] / usa_df['total_births_by_sex'], 0)
+    usa_df = calculate_frequency(usa_df)
 
     # 5. Determine and display a plot consisting of two subplot, where the x-axis is the time scale and the y-axis represents:
     #    - the number of births for each year (top subplot)
@@ -123,7 +156,7 @@ def main():
 
     # The function is implemented in the calculate_top_n_names function (above the main function)
 
-    top_1000_usa_names, usa_df = calculate_top_n_names(usa_df, 1000)
+    top_1000_usa_names = calculate_top_n_names(usa_df, 1000)
 
     # 7. Display the changes for the male name John and the first female name in the top-1000 ranking on a single graph
     #    (provide the graph with an appropriate legend):
@@ -180,23 +213,10 @@ def main():
     #    Answer the question by displaying the appropriate text in the script:
     #    “What has changed over the last 140 years in terms of name diversity? Does diversity depend on gender?”
 
-    diversity_usa_pt = usa_df.pivot_table(index=['year', 'sex'], columns='in_top', values='count', aggfunc='sum', fill_value=0)
-    diversity_usa_pt['top_percentage'] = diversity_usa_pt[True] / (diversity_usa_pt[True] + diversity_usa_pt[False])
+    usa_df, top_1000_percentage_usa = calculate_name_diversity(usa_df, top_1000_usa_names, 1000, 'USA')
 
-    reshaped = diversity_usa_pt['top_percentage'].unstack(level='sex')
-    reshaped['difference'] = abs(reshaped['M'] - reshaped['F'])
-
-    max_diff_year = reshaped['difference'].idxmax()
-    max_diff_value = reshaped.loc[max_diff_year, 'difference']
-
-    plt.figure(figsize=(10, 4))
-    plt.plot(reshaped.index, reshaped['M'], 'b', label='Male names')
-    plt.plot(reshaped.index, reshaped['F'], 'r', label='Female names')
-    plt.axvline(max_diff_year, color='g', label='Year with the greatest difference in diversity')
-    plt.title('Percentage of names in the top1000 ranking')
-    plt.xlabel('Year')
-    plt.ylabel('Percentage')
-    plt.legend()
+    max_diff_year = top_1000_percentage_usa['difference'].idxmax()
+    max_diff_value = top_1000_percentage_usa.loc[max_diff_year, 'difference']
 
     print("-------------------------------------------------")
     print("8. Year with the greatest difference in diversity was:", max_diff_year, "and the difference was:", max_diff_value)
@@ -216,8 +236,6 @@ def main():
 
     usa_df['last_letter'] = usa_df['name'].str[-1]
 
-    # # After comparing the execution time of the two methods, the first method (with groupby and normalizing by dividing)
-    # # was chosen because it is faster
     # # Method 1
     # start_time = pd.Timestamp.now()
     # for i in range(100):
@@ -235,6 +253,8 @@ def main():
     # print("Method 2 execution time:", end_time - start_time)
     # Method 1 execution time: 0 days 00:00:29.487999
     # Method 2 execution time: 0 days 00:00:33.662000
+    # # After comparing the execution time of the two methods, the first method (with groupby and normalizing by dividing)
+    # # was chosen because it is faster
 
     last_letter_df = usa_df.groupby(['year', 'sex', 'last_letter'])['count'].sum().unstack(level='last_letter', fill_value=0)
     last_letter_df = last_letter_df.div(last_letter_df.sum(axis=1), axis=0)
@@ -283,17 +303,108 @@ def main():
     # - display these names
     # - plot the trend for these names illustrating the change in the connotation of a given name over the years
 
-    name_ratios_1880_1920 = calculate_name_gender_ratio(usa_df, 1880, 1920)
-    name_ratios_2000_2023 = calculate_name_gender_ratio(usa_df, 2000, 2023)
+    name_ratios_usa_1880_1920 = calculate_name_gender_ratio(usa_df, 1880, 1920, True)
+    name_ratios_usa_2000_2023 = calculate_name_gender_ratio(usa_df, 2000, 2023, True)
 
-    m2f_change = (name_ratios_1880_1920['p_m'] + name_ratios_2000_2023['p_f']) / 2
+    m2f_change = (name_ratios_usa_1880_1920['p_m'] + name_ratios_usa_2000_2023['p_f']) / 2
     max_m2f_change_name = m2f_change.idxmax()
     max_m2f_change_value = m2f_change.max()
 
     max_f2m_change_name = m2f_change.idxmin()
-    max_f2m_change_value = -(1 - m2f_change.min())
-    print(max_m2f_change_name, max_m2f_change_value)
-    print(max_f2m_change_name, max_f2m_change_value)
+    max_f2m_change_value = (1 - m2f_change.min())
+
+    name_trend_usa_df = usa_df[(usa_df['name'] == max_m2f_change_name) | (usa_df['name'] == max_f2m_change_name)]
+    name_trend_usa_df = name_trend_usa_df.groupby(['year', 'name', 'sex'])['count'].sum()
+    name_trend_usa_df = name_trend_usa_df.unstack(level='sex')
+    name_trend_usa_df = name_trend_usa_df.div(name_trend_usa_df.sum(axis=1), axis=0)
+    name_trend_usa_df = name_trend_usa_df.unstack(level='name')
+
+    # plt.figure(figsize=(10, 4))
+    name_trend_usa_df['M'].plot(kind='line', figsize=(10, 4))
+    plt.ylabel('p_m')
+    plt.xlabel('Year')
+    plt.title('Trend of the names with the largest change in connotation')
+
+    print('-------------------------------------------------')
+    print('10. Name with the largest change from being a female name to a male name is:', max_f2m_change_name,
+          'and the change of p_m is:', max_f2m_change_value)
+    print('Name with the largest change from being a male name to a female name is:', max_m2f_change_name,
+          'and the change of p_m is:', -max_m2f_change_value)
+
+    ### Output:
+    ### 10. Name with the largest change from being a female name to a male name is: Donnie and the change of p_f is: -0.8817976850735472
+    ### Name with the largest change from being a male name to a female name is: Ashley and the change of p_f is: 0.9933546933773039
+
+    # 11. Load a dataset from the database names_pl_2000-23.sqlite containing the number of names given
+    #     in the period 2000-2023 in Poland. The sql query should create a single table containing the name, year,
+    #     and the number of names given for girls and boys. There are 2 separate tables in the database for each gender.
+
+    conn = sqlite3.connect('data/names_pl_2000-23.sqlite')
+    query = """
+            SELECT Rok AS year, Imię AS name, Liczba AS count, 
+                   CASE Płeć WHEN 'K' THEN 'F' ELSE Płeć END AS sex 
+            FROM females
+            UNION ALL
+            SELECT Rok AS year, Imię AS name, Liczba AS count, Płeć AS sex
+            FROM males
+        """
+    pl_df = pd.read_sql_query(query, conn)
+    conn.close()
+
+    # 12. Create a ranking of the top 200 names and compare whether the observations from task 8.
+    #     regarding trends in naming in the USA are also observable in Poland.
+    #     Take 2000, 2013, 2023 as reference years.
+    #     Using a histogram, try to answer the question of what changed between 2000 and 2013,
+    #     whether the change in the trend results only from changing naming trends or other factors.
+
+    pl_df = calculate_frequency(pl_df)
+    top_200_pl_names = calculate_top_n_names(pl_df, 200)
+    pl_df, top_200_percentage_pl = calculate_name_diversity(pl_df, top_200_pl_names, 200, 'Poland')
+
+    fig, axs = plt.subplots(3, 2, figsize=(13, 9))
+    plt.subplots_adjust(hspace=0.5)
+    for i, year in enumerate([2000, 2013, 2023]):
+        pl_df_year = pl_df[pl_df['year'] == year]
+
+        # Plot histogram
+        for j, gender in enumerate(['F', 'M']):
+            if gender == 'F':
+                gender_full = 'female'
+            else:
+                gender_full = 'male'
+
+            bins = np.histogram_bin_edges(pl_df_year[f'frequency_{gender_full}'], bins=20)
+            axs[i, j].hist(pl_df_year[(pl_df_year['sex'] == gender) & (pl_df_year['in_top'] == False)]
+                           [f'frequency_{gender_full}'], bins=bins, color='b', alpha=0.5, label='Not in top')
+            axs[i, j].hist(
+                pl_df_year[(pl_df_year['sex'] == gender) & (pl_df_year['in_top'])][f'frequency_{gender_full}'],
+                bins=bins, color='r', alpha=0.5, label='In top')
+            axs[i, j].set_title(f'Frequency distribution of {gender_full} names in {year}')
+            axs[i, j].set_xlabel('Frequency')
+            axs[i, j].set_ylabel('Number of names')
+            axs[i, j].legend()
+
+    # 13. Find 2 names that were relatively often given to girls and boys in Poland.
+
+    name_ratios_pl_2000_2023 = calculate_name_gender_ratio(pl_df, 2000, 2023, False)
+    name_ratios_pl_2000_2023['ratio'] = abs(name_ratios_pl_2000_2023['p_m'] - name_ratios_pl_2000_2023['p_f'])
+    name_ratios_pl_2000_2023 = name_ratios_pl_2000_2023[name_ratios_pl_2000_2023['ratio'] < 0.50]
+    name_ratios_pl_2000_2023['total_count'] = name_ratios_pl_2000_2023['count']['F'] + name_ratios_pl_2000_2023['count']['M']
+    names_with_similar_ratio = name_ratios_pl_2000_2023.nlargest(2, 'total_count').index.to_list()
+
+    print("-------------------------------------------------")
+    print("13. Names that were relatively often given to both boys and girls in Poland:",
+          names_with_similar_ratio[0], "and", names_with_similar_ratio[1])
+
+    ### Output:
+    ### 13. Names that were relatively often given to both boys and girls in Poland: ANDREA YUVAL
+
+    test_end_time = pd.Timestamp.now()
+    print("-------------------------------------------------")
+    print("Script execution time:", test_end_time - test_time)
+    ### Output:
+    ### Script execution time: 0 days 00:00:09.183000
+    ### On average the script execution time is around 9.2 seconds on Ryzen 5 5600X
 
     plt.show()
     
